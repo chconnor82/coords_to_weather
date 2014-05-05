4ff14eb46f7df61ea811effadd71abee42ca2fb9
Charlie Connor <chconnor@gmail.com>
2014-05-04 23:19:09 -0500

    Final Submission
    Cannot get Nil:NilClas

--- app/controllers/coords_controller.rb
+++ app/controllers/coords_controller.rb
@@ -3,29 +3,28 @@ require 'json'
 
 class CoordsController < ApplicationController
   def fetch_weather
-    @latitude = 42.0538387
-    @longitude = -87.67721
-    @address = (@latitude), (@longitude)
-    your_api_key = "809482e33d1875804a3daa461c6be8f3"
-    @url_safe_address = URI.encode(@address)
+
+    @address_1 = params[:address_1]
+    @url_safe_address = URI.encode(@address_1)
 
     # Your code goes here.
-    url = "http://api.forecast.io/forecast/(your_api_key)/(@latitude),(@longitude)"
-    require 'open["URI.encode(@address)"]'
-    raw_data = open(@url_safe_address).read
+    url = "http://maps.googleapis.com/maps/api/geocode/json?address=#{@url_safe_address}&sensor=false"
+    raw_data = open(url).read
     parsed_data = JSON.parse(raw_data)
-    @temperature = parsed_data["results"][0]["geometry"]["location"][""]
-    @minutely_summary = parsed_data["results"][0]["geometry"]["location"][""]
-    @hourly_summary = parsed_data["results"][0]["geometry"]["location"][""]
-    @daily_summary = parsed_data["results"][0]["geometry"]["location"][""]
+    @latitude = parsed_data["results"][0]["geometry"]["location"]["lat"]
+    @longitude = parsed_data["results"][0]["geometry"]["location"]["lng"]
 
-    # url = ?
-    # raw_data = ?
-    # parsed_data = ?
-    # @temperature = ?
-    # @minutely_summary = ?
-    # @hourly_summary = ?
-    # @daily_summary = ?
+    # @latitude = 42.0538387
+    # @longitude = -87.67721
+    your_api_key = "809482e33d1875804a3daa461c6be8f3"
+
+    url = "https://api.forecast.io/forecast/#{your_api_key}/#{@latitude},#{@longitude}"
+    raw_data = open(url).read
+    parsed_data = JSON.parse(raw_data)
+    @temperature = parsed_data["currently"]["temperature"]
+    @minutely_summary = parsed_data["minutely"]["summary"]
+    @hourly_summary = parsed_data["hourly"]["summary"]
+    @daily_summary = parsed_data["daily"]["summary"]
   end
 end
 
